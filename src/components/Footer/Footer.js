6876f39b74ce1b44c7120798dcf9aa9d26e074dd
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-09-24 00:58:09 +0530

    hh

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Logo from '../../images/logo.png'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3'>
+            <img src={Logo} alt=''/>
+          </div>
+
+          <div className='col-md-3'>
+
+          </div>
+
+          <div className='col-md-3'>
+
+          </div>
+
+          <div className='col-md-3'>
+
+          </div>
+
+        </div>
+
+      </div>
+      </footer>
   )
 }
 
