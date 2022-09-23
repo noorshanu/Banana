3ec42c8f6f0b6de17a1acf13c680b850108c1f39
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-09-24 01:10:02 +0530

    hj

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../../images/logo.png'
+import Insta from '../../images/icon/insta.png'
 import './Footer.css'
 
 function Footer() {
@@ -12,14 +13,40 @@ function Footer() {
           </div>
 
           <div className='col-md-3'>
+            <div>
+              <a href='/'><p>How to Play</p></a>
+              <a href='/'><p>Whitepaper</p></a>
+              <a href='/'><p>Features</p></a>
+              <a href='/'><p>Blog</p></a>
+              <a href='/'><p>Pitchdeck</p></a>
+            </div>
+          </div>
+
+          <div className='col-md-3'>
+          <div>
+              <a href='/'><p>About Us</p></a>
+              <a href='/'><p>Disclaimer</p></a>
+              <a href='/'><p>FAQ</p></a>
+              <a href='/'><p>Cookies Policy</p></a>
+              <a href='/'><p>Privacy Policy</p></a>
+              <a href='/'><p>Terms Of Service</p></a>
+            </div>
 
           </div>
 
           <div className='col-md-3'>
+            <div>
+              <span>Subscribe to our newsletter to get all the updates and announcements</span>
+              <div className='d-flex'>
+                <input type="text" name="" value=""/>
+                <button>Subscribe</button>
 
-          </div>
+              </div>
+              <div className='footer-social'>
+                <a href='/'><img src={Insta} alt=''/></a>
 
-          <div className='col-md-3'>
+              </div>
+            </div>
 
           </div>
 
